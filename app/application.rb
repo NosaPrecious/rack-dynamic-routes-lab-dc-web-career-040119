b6a4ce427bd094f8aedb43259c01d8ad9f049dfa
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-24 22:26:52 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+  
+  @@items_arr = [Item.new("pencil", 5), Item.new("book", 10), Item.new("eraser", 3)]
+  
+  def call(env)
+    
+    resp= Rack::Response.new
+    req= Rack::Request.new(env)
+  
+    if req.path.match(/items/)
+      item_str= req.path.split("/items/").last
+      found_item= @@items_arr.find{|item| item.name == item_str}
+      resp.write found_item.name
+    else
+      resp.write "Page not found"
+      resp.status = 404
+    end
+  end
+  
+end
